2b17f15e2a8b6ce9a72592cf854ca35f2052bfa0
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-02-14 02:28:20 -0800

    whatsapp ui

--- lib/profile-screen.dart
+++ lib/profile-screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:video_player/video_player.dart' as video_player;
+import 'package:video_player/video_player.dart';
+import 'package:video_player/video_player.dart';
 
 class WhatsApp extends StatelessWidget {
   const WhatsApp({super.key});
@@ -34,6 +36,8 @@ class WhatsAppUI extends StatefulWidget {
 }
 
 class _WhatsAppUIState extends State<WhatsAppUI> {
+  int _selectedIndex = 0;
+
   final List<String> names = [
     'Ahmed',
     'Fatima',
@@ -58,6 +62,32 @@ class _WhatsAppUIState extends State<WhatsAppUI> {
     'https://randomuser.me/api/portraits/women/4.jpg',
     'https://randomuser.me/api/portraits/men/5.jpg',
     'https://randomuser.me/api/portraits/women/5.jpg',
+  ];
+
+  final List<String> videos = [
+    'https://www.w3schools.com/html/mov_bbb.mp4',
+    'https://www.w3schools.com/html/movie.mp4',
+    'https://www.w3schools.com/html/mov_bbb.mp4',
+    'https://www.w3schools.com/html/movie.mp4',
+    'https://www.w3schools.com/html/mov_bbb.mp4',
+    'https://www.w3schools.com/html/movie.mp4',
+    'https://www.w3schools.com/html/mov_bbb.mp4',
+    'https://www.w3schools.com/html/movie.mp4',
+    'https://www.w3schools.com/html/mov_bbb.mp4',
+    'https://www.w3schools.com/html/movie.mp4',
+  ];
+
+  final List<String> messages = [
+    'Hey, how are you?',
+    'Let\'s catch up soon!',
+    'Did you see the game last night?',
+    'Happy Birthday!',
+    'Good morning!',
+    'What\'s up?',
+    'Long time no see!',
+    'Can you send me the report?',
+    'Let\'s meet for lunch.',
+    'Have a great day!'
   ];
 
   TextEditingController searchController = TextEditingController();
@@ -85,6 +115,12 @@ class _WhatsAppUIState extends State<WhatsAppUI> {
 
     setState(() {
       filteredNames = results;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -135,69 +171,192 @@ class _WhatsAppUIState extends State<WhatsAppUI> {
           ),
         ],
       ),
-      body: Column(
+      body: IndexedStack(
+        index: _selectedIndex,
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: filteredNames.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(images[index]),
-                  ),
-                  title: Text(filteredNames[index]),
-                  subtitle: Text('Last message from ${filteredNames[index]}'),
-                  trailing: Text('12:00 PM'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ContactScreen(
-                          name: filteredNames[index],
-                          imageUrl: images[index],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            color: Colors.grey[200],
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.emoji_emotions),
-                  onPressed: () {},
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: searchController,
-                    decoration: const InputDecoration(
-                      hintText: 'Type a message',
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.attach_file),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.camera_alt),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
+          ChatsScreen(names: filteredNames, images: images, messages: messages),
+          StatusScreen(names: filteredNames, images: images, videos: videos),
+          CallsScreen(names: filteredNames, images: images),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.circle),
+            label: 'Status',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: 'Calls',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF075E54),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class ChatsScreen extends StatelessWidget {
+  final List<String> names;
+  final List<String> images;
+  final List<String> messages;
+
+  const ChatsScreen(
+      {super.key,
+      required this.names,
+      required this.images,
+      required this.messages});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: names.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(images[index]),
+          ),
+          title: Text(names[index]),
+          subtitle: Text(messages[index]),
+          trailing: Text('12:00 PM'),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatDetailScreen(
+                  name: names[index],
+                  imageUrl: images[index],
+                  message: messages[index],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class StatusScreen extends StatelessWidget {
+  final List<String> names;
+  final List<String> images;
+  final List<String> videos;
+
+  const StatusScreen(
+      {super.key,
+      required this.names,
+      required this.images,
+      required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: Stack(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(images[0]),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: CircleAvatar(
+                  radius: 10,
+                  backgroundColor: Colors.green,
+                  child: Icon(
+                    Icons.add,
+                    size: 15,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          title: Text('My Status'),
+          subtitle: Text('Tap to add status update'),
+          onTap: () {
+            // Handle adding status
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('Recent updates'),
+        ),
+        ...List.generate(names.length, (index) {
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: NetworkImage(images[index]),
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.black.withOpacity(0.5),
+                child: CircularProgressIndicator(
+                  value: 0.7, // Example progress value
+                  strokeWidth: 2,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                ),
+              ),
+            ),
+            title: Text(names[index]),
+            subtitle: Text('Today, 12:00 PM'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StatusDetailScreen(
+                    name: names[index],
+                    imageUrl: images[index],
+                    videoUrl: videos[index],
+                  ),
+                ),
+              );
+            },
+          );
+        }),
+      ],
+    );
+  }
+}
+
+class CallsScreen extends StatelessWidget {
+  final List<String> names;
+  final List<String> images;
+
+  const CallsScreen({super.key, required this.names, required this.images});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: names.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(images[index]),
+          ),
+          title: Text(names[index]),
+          subtitle: Text('Last call from ${names[index]}'),
+          trailing: Icon(Icons.call),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ContactScreen(
+                  name: names[index],
+                  imageUrl: images[index],
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
@@ -324,6 +483,107 @@ class ContactScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ChatDetailScreen extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+  final String message;
+
+  const ChatDetailScreen(
+      {super.key,
+      required this.name,
+      required this.imageUrl,
+      required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(imageUrl),
+                    ),
+                    title: Text(name),
+                    subtitle: Text(message),
+                  ),
+                ],
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Type a message',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StatusDetailScreen extends StatefulWidget {
+  final String name;
+  final String imageUrl;
+  final String videoUrl;
+
+  const StatusDetailScreen(
+      {super.key,
+      required this.name,
+      required this.imageUrl,
+      required this.videoUrl});
+
+  @override
+  _StatusDetailScreenState createState() => _StatusDetailScreenState();
+}
+
+class _StatusDetailScreenState extends State<StatusDetailScreen> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: video_player.VideoPlayer(_controller),
+              )
+            : CircularProgressIndicator(),
       ),
     );
   }
