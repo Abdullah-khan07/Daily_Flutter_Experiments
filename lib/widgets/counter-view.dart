a70333b69047ec72460526b867b76a112fe30811
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-02-27 00:57:13 -0800

    counter increment

--- lib/widgets/counter-view.dart
+++ lib/widgets/counter-view.dart
@@ -8,14 +8,12 @@ class Counterview extends StatefulWidget {
 }
 
 class _CounterviewState extends State<Counterview> {
-  int value1 = 0;
-  int value2 = 0;
-  int sum = 0;
-  sumtwovalue() {
+  int counter = 0;
+
+  void incrementCounter() {
     setState(() {
-      sum = value1 + value2;
+      counter++;
     });
-    print(sum);
   }
 
   @override
@@ -24,32 +22,16 @@ class _CounterviewState extends State<Counterview> {
       body: SafeArea(
         child: Column(
           children: [
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter first value'),
-              onChanged: (text) {
-                value1 = int.tryParse(text) ?? 0;
-              },
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Enter second value'),
-              onChanged: (text) {
-                value2 = int.tryParse(text) ?? 0;
-              },
+            Text(
+              "Counter: $counter",
+              style: TextStyle(fontSize: 27),
             ),
             ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  sumtwovalue();
-                });
-                print(sum);
-              },
-              child: Text("Calculate Sum"),
-            ),
-            Text(
-              "Sum = $sum",
-              style: TextStyle(fontSize: 35),
+              onPressed: incrementCounter,
+              child: Text(
+                "Increment",
+                style: TextStyle(fontSize: 20),
+              ),
             ),
           ],
         ),
