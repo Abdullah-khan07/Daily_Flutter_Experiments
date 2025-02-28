a5ab6c62ba1d8266e220c1a35039d9339dbe76b7
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-02-27 19:45:06 -0800

    list view.builder paractice

--- lib/widgets/counter-view.dart
+++ lib/widgets/counter-view.dart
@@ -20,18 +20,48 @@ class _CounterviewState extends State<Counterview> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
           children: [
-            Text(
-              "Counter: $counter",
-              style: TextStyle(fontSize: 27),
-            ),
-            ElevatedButton(
-              onPressed: incrementCounter,
-              child: Text(
-                "Increment",
-                style: TextStyle(fontSize: 20),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Counter: $counter",
+                    style: TextStyle(fontSize: 27),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: incrementCounter,
+                    child: Text(
+                      "Increment",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
               ),
+            ),
+            SizedBox(height: 20),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(bottom: 0.5),
+                  child: ListTile(
+                    tileColor: Color.fromARGB(255, 57, 53, 99),
+                    title: Text(
+                      'Item $index',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      'khan hai larke',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
             ),
           ],
         ),
